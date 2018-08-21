6eca7dd08052b8c768bfabc9edbb5314106c5a95
Andres Ferragut <andres.ferragut@gmail.com>
2018-08-21 16:00:18 -0300

    Final sim for EPIM paper added.

--- google_data/google_data_sim.jl
+++ google_data/google_data_sim.jl
@@ -1,5 +1,5 @@
 push!(LOAD_PATH,"simulator")
-using EVQueues, CSV, DataFrames, PyPlot
+using EVQueues, CSV, DataFrames
 
 data = CSV.read("google_data/Google_Test_Data_filtered.csv",
                 datarow=2,
@@ -9,8 +9,11 @@ data = CSV.read("google_data/Google_Test_Data_filtered.csv",
 #ordeno por arribo
 sort!(data, [:arriboAbsoluto]);
 
+
+Tfinal = 86400.0;
+
 #filtro 2 dias de autos
-idx = data[:arriboAbsoluto].<(86400);
+idx = data[:arriboAbsoluto].<Tfinal;
 
 arribos = data[idx,:arriboAbsoluto];
 partidas = data[idx,:arriboAbsoluto]+data[idx,:permanencia];
@@ -19,24 +22,25 @@ trabajos = data[idx,:tiempoCarga];
 C=30;
 
 #simula usando edf a partir de la traza. Cambiar edf por llf, llr, pf, parallel para las otras politicas.
-sim = ev_llr_trace(arribos,trabajos,partidas,C)
-compute_statistics!(sim)
+sim_llr = ev_llr_trace(arribos,trabajos,partidas,C)
+sim_llf = ev_llf_trace(arribos,trabajos,partidas,C)
+sim_edf = ev_edf_trace(arribos,trabajos,partidas,C)
 
-#Ploteo la salida como escalera. where=post es para que se mantenga constante a la derecha del intervalo.
-#sim.X tiene los vehiculos cargando.
-#sim.Y tiene los vehiculos finalizados.
-PyPlot.step(sim.T,sim.X,where="post")
-PyPlot.step(sim.T,sim.Y,where="post")
+t=collect(60:60:Tfinal);
+#media hora de ventana
+h=1800.0;
 
-figure()
+J_llr=compute_fairness(sim_llr,t,h);
+J_llf=compute_fairness(sim_llf,t,h);
+J_edf=compute_fairness(sim_edf,t,h);
 
-worig = sim.W[:,1];
-sworig=sort(worig);
-k=length(sworig);
-PyPlot.plot(sworig,(1:k)/k,drawstyle="steps")
+using PGFPlots
 
-figure()
-rW = sim.W[:,2];
-sW=sort(rW);
-k=length(sW);
-PyPlot.plot(sW,(1:k)/k,drawstyle="steps")
+
+fig = Axis([
+    Plots.Linear(t/86400*24,J_llr, legendentry="LLR"),
+    Plots.Linear(t/86400*24,J_llf, legendentry="LLF"),
+    Plots.Linear(t/86400*24,J_edf, legendentry="EDF"),
+    ], legendPos="north east", xlabel="time (hours)", ylabel="Fairness index", xmin=0.0, xmax=24, ymin=0, ymax=1.1)
+
+save("fairness_index.tex",fig,include_preamble=false)
