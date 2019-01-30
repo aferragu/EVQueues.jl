365a6c3ef013a37bfad24a7f5b0289e9e019f8e1
Andres Ferragut <andres.ferragut@gmail.com>
2019-01-30 11:39:33 -0300

    Restored attained work examples lost in backup.

---
+++ sims_attained/example_animation.jl
@@ -0,0 +1,42 @@
+push!(LOAD_PATH,"simulator")
+using EVQueues, Plots, ProgressMeter
+
+lambda=120.0;
+mu=1.0;
+gamma=0.5;
+#C=80.0;
+C=60.0;
+
+Tfinal=60.0;
+
+frames = 24*45;
+
+snaps = collect(range(0.01,stop=Tfinal,length=frames));
+
+sim = ev_lifo(lambda,mu,gamma,Tfinal,C,snapshots=snaps)
+compute_statistics!(sim)
+
+prog=Progress(length(snaps), dt=1, desc="Creando animacion... ");
+
+azulcito = RGB(0.0,140/256,240/256);
+
+rojito = RGB(240/256,0.0,140/256);
+
+
+anim = @animate for i=1:length(snaps)
+
+    w = [ev.requestedEnergy - ev.currentWorkload for ev in sim.snapshots[i].charging];
+    d = [ev.departureTime - ev.arrivalTime - ev.currentDeadline for ev in sim.snapshots[i].charging];
+    u = [ev.currentPower>0 for ev in sim.snapshots[i].charging];
+
+    p=scatter(w[u.>0],d[u.>0],markershape=:circle,color=azulcito,label="En servicio",xlims = (0,4), ylims = (0,3), size=(1000,600));
+    scatter!(p,w[u.==0],d[u.==0],markershape=:circle,color=rojito,label="En espera");
+
+    xlabel!(p,"Carga alcanzada");
+    ylabel!(p,"Tiempo transcurrido");
+    title!(p,"Población EV")
+
+    next!(prog);
+end
+
+gif(anim, "sims_attained/lifo.gif", fps = 24)
---
+++ sims_attained/example_attained.jl
@@ -0,0 +1,80 @@
+push!(LOAD_PATH,"simulator")
+using EVQueues, Plots
+
+lambda=120.0;
+mu=1.0;
+gamma=0.5;
+C=60.0;
+
+Tfinal=1000.0;
+
+
+sim = ev_lifo(lambda,mu,gamma,Tfinal,C,snapshots=[Tfinal])
+compute_statistics!(sim)
+
+
+#TIme plot of occupation and power
+p1 = plot(  xlabel="Time",
+            ylabel="# vehicles",
+            title="Vehicles in charge")
+
+plot!(p1, sim.timetrace.T, sim.timetrace.X,lt=:steppost,linewidth=2,legend=:none);
+
+p2 = plot(  xlabel="Time",
+            ylabel="# vehicles",
+            title="Vehicles already charged")
+
+plot!(p2, sim.timetrace.T, sim.timetrace.Y,lt=:steppost,linewidth=2,legend=:none);
+
+p3 = plot(  xlabel="Time",
+            ylabel="P (kW)",
+            title="Consumed power")
+
+plot!(p3, sim.timetrace.T, sim.timetrace.P,lt=:steppost,linewidth=2,legend=:none);
+
+l=@layout [a;b;c];
+p=plot(p1,p2,p3,layout=l)
+display(p)
+
+#CDF of departure attained workloads
+
+Sa = sort([ev.departureWorkload for ev in sim.EVs]);
+n = length(Sa);
+p = plot(   xlabel="w (kWh)",
+            ylabel="P(Sa⩽w)",
+            title="Attained work CDF")
+
+plot!(p,Sa,(1:n)/n,lt=:steppost,legend=:none)
+display(p)
+
+#State space of the last snapshot
+snap = sim.snapshots[end];
+
+w = [ev.requestedEnergy - ev.currentWorkload for ev in snap.charging];
+d = [ev.departureTime - ev.arrivalTime - ev.currentDeadline for ev in snap.charging];
+on = [ev.currentPower>0 for ev in snap.charging];
+
+p = plot(   xlabel = "Remaining workload",
+            ylabel = "Remaining soj. time",
+            title = "State-space snapshot")
+
+scatter!(p,w[on.==true],d[on.==true],markershape=:circle,markersize=4,color=:blue,label="In service")
+scatter!(p,w[on.==false],d[on.==false],markershape=:circle,markersize=4,color=:red,label="Not in service")
+
+display(p)
+
+#fairness measure over time
+
+t=collect(0:10:Tfinal);
+h=20.0;
+J=compute_fairness(sim,t,h)
+
+p = plot(   xlabel = "Time",
+            ylabel = "J",
+            title = "Fairness measure",
+            ylim = (0,1)
+            )
+
+plot!(p,t,J,lw=2,legend=:none)
+
+display(p)
