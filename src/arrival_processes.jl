d79430c5a51cf470ab6680b32d797f930c0d563d
Andres Ferragut <andres.ferragut@gmail.com>
2022-08-22 11:56:18 -0300

    Updated tracing ArrivalProcess

--- src/arrival_processes.jl
+++ src/arrival_processes.jl
@@ -5,10 +5,18 @@ function connect!(arr::ArrivalProcess, a::Agent)
     arr.sink = a
 end
 
+#update state after dt time units
+function update_state!(arr::ArrivalProcess, dt::Float64)
+    arr.timeToNextEvent = arr.timeToNextEvent-dt
+end
+
+function trace_state!(arr::ArrivalProcess, t::Float64)
+    push!(arr.trace, [t,arr.totalArrivals, arr.totalEnergy])
+end
+
 
 ### Poisson Arrival Process
 #   Random arrivals as a Poisson process with general deadline and work distributions.
-
 mutable struct PoissonArrivalProcess <: ArrivalProcess
 
     #attributes
@@ -25,25 +33,18 @@ mutable struct PoissonArrivalProcess <: ArrivalProcess
     nextEventType::Symbol
 
     #tracing
+    trace::DataFrame
     totalArrivals::Float64
     totalEnergy::Float64
 
     function PoissonArrivalProcess(intensity::Float64,requestedEnergy::Distribution, initialLaxity::Distribution, chargingPower::Union{Distribution,Float64})
 
         firstArrival = rand(Exponential(1/intensity))
-        new(intensity, requestedEnergy, initialLaxity, chargingPower, nothing, firstArrival,:Arrival, 0.0,0.0)
+        trace = DataFrame(time=[0.0], totalArrivals=[0.0], totalEnergy=[0.0])
+        new(intensity, requestedEnergy, initialLaxity, chargingPower, nothing, firstArrival,:Arrival, trace, 0.0,0.0)
 
     end
 
-end
-
-#update state after dt time units
-function update_state!(arr::PoissonArrivalProcess, dt::Float64)
-    arr.timeToNextEvent = arr.timeToNextEvent-dt
-end
-
-function get_traces(arr::PoissonArrivalProcess)::Vector{Float64}
-    return [arr.totalArrivals, arr.totalEnergy]
 end
 
 #handles the event at time t with type "event"
@@ -69,10 +70,10 @@ function handle_event(arr::PoissonArrivalProcess, t::Float64, params...)
 
 end
 
+
 ### Trace Arrival process
 #   Given a trace (vectors or DataFrame) of arrival times, energies, deadlines and charging powers,
 #   constructs the corresponding arrival process.
-
 mutable struct TraceArrivalProcess <: ArrivalProcess
 
     #attributes
@@ -89,45 +90,31 @@ mutable struct TraceArrivalProcess <: ArrivalProcess
     nextEventType::Symbol
 
     #tracing
+    trace::DataFrame
     totalArrivals::Float64
     totalEnergy::Float64
 
     function TraceArrivalProcess(arrivalTimes::Vector{Float64}, requestedEnergies::Vector{Float64} ,departureTimes::Vector{Float64}, chargingPowers::Vector{Float64})
 
-        nextArrival = rand(Exponential(1/intensity))
         @assert issorted(arrivalTimes) "Arrival times must be sorted"
-        new(arrivalTimes, requestedEnergies, departureTimes, chargingPowers, nothing, nextArrival,0.0,0.0)
+        trace = DataFrame(time=[0.0], totalArrivals=[0.0], totalEnergy=[0.0])
+        new(arrivalTimes, requestedEnergies, departureTimes, chargingPowers, nothing, arrivalTimes[1],:Arrival,trace,0.0,0.0)
 
     end
 
     function TraceArrivalProcess(data::DataFrame)
 
         sort!(data,:arrivalTimes)
-        nextArrival = data[:arrivalTimes][1]
-        new(data[:arrivalTimes], data[:requestedEnergies], data[:departureTimes], data[:chargingPowers], nothing, nextArrival,0.0,0.0)
+        new(data[:arrivalTimes], data[:requestedEnergies], data[:departureTimes], data[:chargingPowers])
 
     end
 
 end
 
-#update state after dt time units
-function update_state!(arr::TraceArrivalProcess, dt::Float64)
-    arr.nextArrival = arr.nextArrival-dt
-end
-
-function get_traces!(arr::TraceArrivalProcess)::Vector{Float64}
-    return [arr.totalArrivals, arr.totalEnergy]
-end
-
-#returns the time of next event and names
-function get_next_event(arr::TraceArrivalProcess)::Tuple{Float64,Symbol}
-    return arr.timeToNextEvent,arr.nextEventType
-end
-
 #handles the event at time t with type "event"
 function handle_event(arr::TraceArrivalProcess, t::Float64, params...)
     
-    @assert isapprox(arr.timeToNextEvent,0.0,atol=eps()) "Called handle_event in ArrivalProcess but nextArrival>0"
+    @assert isapprox(arr.timeToNextEvent,0.0,atol=eps()) "Called handle_event in TraceArrivalProcess but nextArrival>0"
     arr.totalArrivals = arr.totalArrivals + 1
 
     energy = arr.requestedEnergies[arr.totalArrivals]
