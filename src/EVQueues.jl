715fb1fa1ca2c12b0c271cb1b23d471d265edf22
Andres Ferragut <andres.ferragut@gmail.com>
2022-08-22 12:20:56 -0300

    reordered evqueues

--- src/EVQueues.jl
+++ src/EVQueues.jl
@@ -6,19 +6,22 @@ export  compute_statistics!, get_vehicle_trajectories, compute_fairness, generat
         EVSim, loadsim, savesim
 
 
-include("types.jl") ##Type definitions
+include("types.jl") ##General type definitions
+include("arrival_processes.jl")
+include("charging_stations.jl")
+include("routing_agents.jl")
+
 include("utilities.jl") ##codigo con utilidades varias
+include("policies.jl")  ##codigo que implementa las politicas
+
 include("ev_sim.jl")  ##codigo del simulador comun
 include("ev_sim_trace.jl") ##codigo del simulador a partir de trazas
 include("ev_sim_uncertain.jl") ##codigo del simulador con incertidumbre en el deadline
-include("policies.jl")  ##codigo que implementa las politicas
-include("plot_recipes.jl") ##plots
 
 include("ev_sim_two_parkings.jl")
 include("ev_sim_three_phase.jl")
 
-include("arrival_processes.jl")
-include("charging_stations.jl")
-include("routing_agents.jl")
+include("plot_recipes.jl") ##plots
+
 
 end #end module
