88781f27b95ed214536da3c315a3ed7adc10b221
Andres Ferragut <andres.ferragut@gmail.com>
2022-08-22 15:04:10 -0300

    Updated ev_sim and ev_sim_trace helpers

--- src/ev_sim.jl
+++ src/ev_sim.jl
@@ -1,8 +1,5 @@
 function ev_sim(lambda,mu,gamma,Tfinal,C,policy,snapshots=[Inf])
 
-    #barra de progreso
-    prog=Progress(101, dt=0.5, desc="Simulando... ");
-
     #guardo parametros
     params = Dict(
         "ArrivalRate" => lambda,
@@ -21,80 +18,11 @@ function ev_sim(lambda,mu,gamma,Tfinal,C,policy,snapshots=[Inf])
 
     #Proceso de arribos
     arr = PoissonArrivalProcess(lambda,work_rng,laxity_rng,1.0)
-    sta = ChargingStation(10000,C,policy)
+    sta = ChargingStation(Inf,C,policy)
     connect!(arr,sta)
-    agents = [arr,sta]::Vector{Agent}
-    #valores para traza
-    num=convert(Integer,round(3.3*lambda*Tfinal)+length(snapshots));
-    T=zeros(num);
-    X=zeros(UInt16,length(T));   #charging vehicles
-    Y=zeros(UInt16,length(T));   #already charged
-    P=zeros(Float64,length(T));   #used power
+    sim = Simulation([arr,sta])
 
-    snaps = Array{Snapshot}(undef,0);
-
-    t=0.0
-    T[1]=t;
-    X[1]=0;
-    Y[1]=0;
-    P[1]=0.0;
-
-    i=1;    #event counter
-    m=1;    #snapshot counter
-
-    ##TODO: consider snapshots
-    nextSnapshot = snapshots[m];
+    simulate(sim, Tfinal; snapshots=snapshots)
+    return sim
     
-    #nextEvent
-    nextEvents = get_next_event.(agents)
-    dt,idx = findmin([u[1] for u in nextEvents])
-    event = nextEvents[idx][2]
-    handler = agents[idx]
-    
-    while t<Tfinal
-
-        t=t+dt;
-        nextSnapshot = nextSnapshot - dt;
-
-        update_state!.(agents, dt)
-
-        handle_event(handler,t,event)
-
-        # snapshot=Snapshot(t,deepcopy(charging),deepcopy(alreadyCharged));
-        # push!(snaps,snapshot);
-
-        # if (m<length(snapshots))
-        #     m=m+1;
-        #     nextSnapshot = snapshots[m]-t;
-        # else
-        #     nextSnapshot = Inf;
-        # end
-
-
-        i=i+1;
-        T[i]=t;
-        X[i]=length(sta.charging);
-        Y[i]=length(sta.alreadyCharged);
-        P[i]=sta.currentPower;
-
-        nextEvents = get_next_event.(agents)
-        dt,idx = findmin([u[1] for u in nextEvents])
-        event = nextEvents[idx][2]
-        handler = agents[idx]
-    
-        progreso = ceil(Integer,t/Tfinal*100);
-        ProgressMeter.update!(prog,progreso);
-
-    end
-
-    T=T[1:i];
-    X=X[1:i];
-    Y=Y[1:i];
-    P=P[1:i];
-
-    next!(prog);
-
-    trace = TimeTrace(T,X,Y,P);
-    stats = SimStatistics([],[],[],[],NaN,NaN,NaN,NaN);
-    return EVSim(params,trace,sta.completedEVs,snaps,stats)
 end
--- src/ev_sim_trace.jl
+++ src/ev_sim_trace.jl
@@ -11,189 +11,34 @@ policy: una de las politicas definidas en EVSim
 snapshots: tiempo de capturas
 salidaReportada: vector opcional que altera los deadlines reportados.
 =#
-function ev_sim_trace(arribos,demandas,salidas,potencias,policy,C,snapshots; salidaReportada=nothing)
-
-    num = length(arribos); #no. de vehiculos a procesar.
-    prog = Progress(num+1, dt=0.5, desc="Simulando... ");
-
-    eventos = 3*num+1+length(snapshots);
-
+function ev_sim_trace(arrivalTimes::Vector{Float64}, 
+                      requestedEnergies::Vector{Float64},
+                      departureTimes::Vector{Float64}, 
+                      chargingPowers::Vector{Float64},
+                      policy::Function,
+                      C::Number, 
+                      snapshots=[Inf])
 
     #guardo parametros
     params = Dict(
-        "TotalArrivals" => length(arribos),
-        "AvgEnergy" => mean(demandas),
-        "AvgDeadline" => mean(salidas-arribos),
-        "SimTime" => salidas[end],
+        "TotalArrivals" => length(arrivalTimes),
+        "AvgEnergy" => mean(requestedEnergies),
+        "AvgDeadline" => mean(departureTimes-arrivalTimes),
+        "SimTime" => departureTimes[end],
         "Capacity" => C,
         "Policy" => get_policy_name(policy),
         "AvgReportedDeadline" => NaN,
         "Snapshots" => length(snapshots)
     )
 
-    if salidaReportada!==nothing
-        params["AvgReportedDeadline"] = mean(salidaReportada-arribos)
-    end
+    arr = TraceArrivalProcess(arrivalTimes,requestedEnergies,departureTimes,chargingPowers)
+    sta = ChargingStation(Inf,C,policy)
+    connect!(arr,sta)
+    sim = Simulation([arr,sta])
 
-    #valores iniciales
-    T=zeros(eventos);
-    X=zeros(UInt16,length(T));   #charging vehicles
-    Y=zeros(UInt16,length(T));   #already charged
-    P=zeros(Float64,length(T));   #used power
+    simulate(sim, Tfinal; snapshots=snapshots)
+    return sim
 
-    snaps = Array{Snapshot}(undef,0);
-
-    t=0.0;
-    x=0;
-    y=0;
-    p=0.0;
-
-    T[1]=t;
-    X[1]=x;
-    Y[1]=y;
-    P[1]=p;
-
-    i=1;    #event counter
-    m=1;    #snapshot counter
-
-    charging = Array{EVinstance}(undef,0);
-    alreadyCharged = Array{EVinstance}(undef,0);
-    finished = Array{EVinstance}(undef,0);
-
-    powerAllocation = Array{Float64}(undef,0);
-
-    arrivals=0;
-    expired=0;
-
-    nextArr = arribos[arrivals+1]-t; #inicializo al primer arribo
-    nextCharge = Inf;
-    nextDepON = Inf;
-    nextDepOFF = Inf;
-    nextSnapshot = snapshots[m];
-
-    dt,caso = findmin([nextArr;nextCharge;nextDepON;nextDepOFF;nextSnapshot])
-
-    while dt<Inf
-
-        t=t+dt;
-        nextArr=nextArr-dt;
-        nextSnapshot = nextSnapshot - dt;
-
-        map(v->update_vehicle(v,dt),charging);
-        map(v->update_vehicle(v,dt),alreadyCharged);
-
-
-        if caso==1          #arribo
-            arrivals=arrivals+1;
-            if arrivals<num
-                nextArr = arribos[arrivals+1]-t;
-            else
-                nextArr = Inf;
-            end
-
-            x=x+1;
-
-            if salidaReportada===nothing
-                push!(charging,EVinstance(t,salidas[arrivals],demandas[arrivals],potencias[arrivals]));
-            else
-                push!(charging,EVinstance(t,salidas[arrivals],salidaReportada[arrivals],demandas[arrivals],potencias[arrivals]));
-            end
-
-        elseif caso==2      #charge completed
-            x=x-1;
-            y=y+1;
-
-            #guardo el auto que termina
-            aux,k = findmin([ev.currentWorkload for ev in charging]); ##TODO agregar un assert de que aux==0?
-            ev = charging[k];
-
-            push!(finished,ev);
-            push!(alreadyCharged,ev);
-            deleteat!(charging,k)
-
-            ev.currentPower=0.0;
-            ev.departureWorkload=0.0;
-            ev.completionTime=t;
-
-        elseif caso==3      #departure without full charge
-            expired=expired+1;
-            x=x-1;
-
-            #guardo el auto que termina
-            aux,k = findmin([ev.currentDeadline for ev in charging]);
-            ev=charging[k];
-
-            push!(finished,ev);
-            deleteat!(charging,k)
-
-            ev.currentPower=0.0;
-            ev.departureWorkload = ev.currentWorkload;
-            ev.completionTime=t;
-
-        elseif caso==4      #departure after full charge
-            y=y-1;
-            aux,k = findmin([ev.currentDeadline for ev in alreadyCharged]);
-            deleteat!(alreadyCharged,k);
-
-        elseif caso==5      #take snapshot
-
-            snapshot=Snapshot(t,deepcopy(charging),deepcopy(alreadyCharged));
-            push!(snaps,snapshot);
-
-            if (m<length(snapshots))
-                m=m+1;
-                nextSnapshot = snapshots[m]-t;
-            else
-                nextSnapshot = Inf;
-            end
-
-        end
-
-        if x>0
-            #Apply the policy to all vehicles. Return total user power
-            p = policy(charging,C);
-
-            if minimum([ev.currentWorkload for ev in charging])==0
-                nextCharge=0;
-            else
-                nextCharge = minimum([ev.currentWorkload/ev.currentPower for ev in charging]);
-            end
-            nextDepON = minimum([ev.currentDeadline for ev in charging]);
-        else
-            nextCharge = Inf;
-            nextDepON = Inf;
-            p=0.0;
-        end
-
-        if y>0
-            nextDepOFF = minimum([ev.currentDeadline for ev in alreadyCharged]);
-        else
-            nextDepOFF = Inf;
-        end
-
-        i=i+1;
-        T[i]=t;
-        X[i]=x;
-        Y[i]=y;
-        P[i]=p;
-
-        dt,caso = findmin([nextArr;nextCharge;nextDepON;nextDepOFF;nextSnapshot])
-
-        ProgressMeter.update!(prog,arrivals);
-
-    end
-
-    T=T[1:i];
-    X=X[1:i];
-    Y=Y[1:i];
-    P=P[1:i];
-
-    pD=expired/arrivals;
-    next!(prog);
-
-    trace = TimeTrace(T,X,Y,P);
-    stats = SimStatistics([],[],[],[],NaN,NaN,NaN,NaN);
-    return EVSim(params,trace,finished,snaps,stats)
 end
 
 
@@ -202,11 +47,17 @@ end
 #Recibe un dataframe que tiene:
 #arribos, demandas, salidas, potencias y opcionalmente salidaReportada
 #se encarga de desarmar el dataframe y llamar a ev_sim_trace anterior
-function ev_sim_trace(df::DataFrame,policy,C,snapshots)
+function ev_sim_trace(  df::DataFrame,
+                        policy::Function,
+                        C::Number,
+                        snapshots=[Inf])
 
-    if "salidaReportada" in names(df)
-        ev_sim_trace(df[!,:arribos],df[!,:demandas],df[!,:salidas],df[!,:potencias],policy,C,snapshots; salidaReportada=df[!,:salidaReportada])
-    else
-        ev_sim_trace(df[!,:arribos],df[!,:demandas],df[!,:salidas],df[!,:potencias],policy,C,snapshots)
-    end
+    ev_sim_trace(   df[!,:arrivalTimes],
+                    df[!,:requestedEnergies],
+                    df[!,:departureTimes],
+                    df[!,:chargingPowers],
+                    policy,
+                    C,
+                    snapshots)
+    
 end
